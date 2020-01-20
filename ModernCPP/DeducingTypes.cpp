1e680c4f4be682d3ea0241c9f71baeafd5ba3035
kek228 <xyets777@gmail.com>
2020-01-20 09:19:56 +0300

    Подключил boost, чтобы выводить красивые типы

--- ModernCPP/DeducingTypes.cpp
+++ ModernCPP/DeducingTypes.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <boost/type_index.hpp>
+using boost::typeindex::type_id_with_cvr;
 
+
+/* ****** template type deducing ****** */
+// T->type, T&->param type (тип со всеми модификаторами итд)
+template<typename T>
+void f(T& param){
+    std::cout<<type_id_with_cvr<T>().pretty_name();
+}
 
 int main() {
+    int x = 1;
+    const int refx = x;
+    const int &crefx = x;
+
+    f(refx);
     return 0;
 }
