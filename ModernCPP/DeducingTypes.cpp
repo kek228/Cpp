b0765ce5e73e5c30264f3f9523deebc978fe3f6e
kek228 <xyets777@gmail.com>
2020-01-20 09:42:32 +0300

    Chapter1/Item1

--- ModernCPP/DeducingTypes.cpp
+++ ModernCPP/DeducingTypes.cpp
@@ -1,20 +1,64 @@
 #include <iostream>
 #include <boost/type_index.hpp>
 using boost::typeindex::type_id_with_cvr;
+using namespace std;
+// ГЛАВА 1. Вывод типов для шаблонов, auto и decltype
+/* ****** ITEM 1: template type deducing ****** */
 
-
-/* ****** template type deducing ****** */
-// T->type, T&->param type (тип со всеми модификаторами итд)
+// CASE1: param type: T&
+// Тип T отбрасывает ссылку, но оставляет const/volatile
 template<typename T>
-void f(T& param){
-    std::cout<<type_id_with_cvr<T>().pretty_name();
+void case1(T& param){
+    cout<<type_id_with_cvr<T>().pretty_name()<<std::endl;
 }
 
-int main() {
+// CASE2: param type: T&& (универсальная ссылка)
+// Все предсказуемо, T теперь просто не отбросит ссылку
+// НО отдельный кейс когда param type: rvalue, T разрезолвится в int
+template<typename T>
+void case2(T&& param){
+    cout<<type_id_with_cvr<T>().pretty_name()<<std::endl;
+}
+
+// CASE3: param type: T
+// вся ссылочность и константность отбросится, ведь param будет копией
+template<typename T>
+void case3(T param){
+    cout<<type_id_with_cvr<T>().pretty_name()<<std::endl;
+}
+
+void item1(){
     int x = 1;
-    const int refx = x;
+    const int cx = 2;
     const int &crefx = x;
 
-    f(refx);
+    // CASE 1: как видно & нигде нет, потому что pt и так ссылка
+    cout<<"CASE1"<<endl;
+    case1(x); // int
+    case1(cx); // int const
+    case1(crefx); // int const
+
+    // CASE 2: теперь & везде есть кроме rvalue, потому что универсальная ссылка должна в что то разрезолвится
+    cout<<"CASE2"<<endl;
+    case2(x); // int&
+    case2(cx); // int const&
+    case2(crefx); // int const&
+    case2(100); // int
+
+    // CASE 3
+    cout<<"CASE3"<<endl;
+    case3(x); // int
+    case3(cx); // int
+    case3(crefx); // int
+    case3(100); // int
+    const char* const ptr = "Fun with pointers";
+    case3(ptr); // char const* вторая константность будет отброшена, ptr скопируется и его можно будет занулить
+    // но не первая, ведь данные по этому адресу менять НЕЛЬЗЯ
+}
+
+
+
+int main() {
+    item1();
     return 0;
 }
