6a721d9f4efe4231be761397379a7f1d26a1919a
kek228 <xyets777@gmail.com>
2020-01-21 22:53:04 +0300

    Chapter1/Item2

--- ModernCPP/DeducingTypes.cpp
+++ ModernCPP/DeducingTypes.cpp
@@ -33,20 +33,23 @@ void item1(){
     const int &crefx = x;
 
     // CASE 1: как видно & нигде нет, потому что pt и так ссылка
-    cout<<"CASE1"<<endl;
+    cout<<"template<typename T>\n"
+          "void case1(T& param){}"<<endl;
     case1(x); // int
     case1(cx); // int const
     case1(crefx); // int const
 
     // CASE 2: теперь & везде есть кроме rvalue, потому что универсальная ссылка должна в что то разрезолвится
-    cout<<"CASE2"<<endl;
+    cout<<"template<typename T>\n"
+          "void case2(T&& param){}"<<endl;
     case2(x); // int&
     case2(cx); // int const&
     case2(crefx); // int const&
     case2(100); // int
 
     // CASE 3
-    cout<<"CASE3"<<endl;
+    cout<<"template<typename T>\n"
+          "void case3(T param){}"<<endl;
     case3(x); // int
     case3(cx); // int
     case3(crefx); // int
@@ -54,11 +57,56 @@ void item1(){
     const char* const ptr = "Fun with pointers";
     case3(ptr); // char const* вторая константность будет отброшена, ptr скопируется и его можно будет занулить
     // но не первая, ведь данные по этому адресу менять НЕЛЬЗЯ
+
+    // Array arg. Что будет если передать T[] или T*
+    cout<<"ARRAY ARG"<<endl;
+    char arr[] = "hello";
+    char *ptr_to_arr = arr;
+    case1(arr); // char [6] ссылочность была отброшена, а тип остался, как и для int-a
+    case1(ptr_to_arr); // char*
+    case2("kek"); // char const (&) [4] // оба ссылки на arr[]
+    case2(arr); // char (&) [6]
+    case2(ptr_to_arr); // char*& какое то очко
+    case3(arr); // char* тут все понятно, это просто скопировался поинтер
+    case3(ptr_to_arr); // char*
 }
+/***** ITEM 2 *****/
+// А теперь тоже самое только для auto
+// Смысл, что auto это как T в функциях case сверху, а Pt это cv с auto и &/*
+// case-ы те же самые выводится тип auto точно также, КРОМЕ случая auto a = {}
+// там auto будет std::initializer_list
+void item2(){
+    int x = 1;
+    const int cx = 2;
+    const int &crefx = x;
+    // CASE 1 auto развернется как ПРОСТО int и оставит cv, в итоге будет:
+    {
+        auto &auto_x = x; // int&
+        auto &auto_cx = cx; // const int&
+        auto &auto_crefx = crefx; // const int&
+    }
+    // CASE 2
+    {
+        auto &&auto_x = x; // int&
+        auto &&auto_cx = cx; // const int&
+        auto &&auto_crefx = crefx; // const int&
+        auto &&rval = 1; // int &&
+    }
+    // CASE 3 везде тупо int
+    {
+        auto auto_x = x; // int
+        auto auto_cx = cx; // int
+        auto auto_crefx = crefx; // int
+        auto rval = 1; // int
+    }
+
+}
+
 
 
 
 int main() {
     item1();
+    item2();
     return 0;
 }
