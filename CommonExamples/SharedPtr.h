37c7319198df3e696b62af433745e7dd45054250
kek228 <xyets777@gmail.com>
2020-03-10 22:43:20 +0300

    SharedPtr работает прям с контрол блоком

--- CommonExamples/SharedPtr.h
+++ CommonExamples/SharedPtr.h
@@ -1,46 +1,53 @@
 #include <iostream>
 #include <atomic>
 #include <memory>
+#include <functional>
 
 template<typename T>
-void defaultDel(T *ptr){
-    delete ptr;
-}
-
-template<typename T>
-struct ControlBlock{
-    ControlBlock():_refCount(0){
-
-    }
-
-    ControlBlock(const int v):_refCount(v){
-
-    }
-    
-    void deleter(T *ptr){
+class DefaultDeleter{
+public:
+    void operator()(T *ptr){
         delete ptr;
     }
+};
 
+template<typename T>
+struct ControlBlock {
+
+    ControlBlock() : _refCount(1), deleter(DefaultDeleter<T>()) {
+
+    }
+
+    template<typename DEL>
+    ControlBlock(DEL del) : _refCount(1), deleter(del) {
+
+    }
+
+    std::function<void(T*)> deleter;
     std::atomic<int> _refCount;
 };
 
 template<typename T>
-class SharedPtr{
+class SharedPtr {
 public:
-    SharedPtr(): _ptr(nullptr), _ctr_block(nullptr){
+    SharedPtr() : _ptr(nullptr), _ctr_block(nullptr) {
+
+    }
+    template<typename Y>
+    SharedPtr(Y *ptr) : _ptr(ptr), _ctr_block(new ControlBlock<T>()) {
 
     }
 
-    SharedPtr(T *ptr): _ptr(ptr), _ctr_block(new ControlBlock<T>(1)) {
-
+    template<typename Y>
+    SharedPtr(const SharedPtr<Y> &r) : _ptr(r._ptr), _ctr_block(r._ctr_block) {
     }
 
-    SharedPtr(const SharedPtr& r): _ptr(r._ptr), _ctr_block(r._ctr_block){
-        ++(_ctr_block->_refCount);
+    template<typename Y, typename DEL>
+    SharedPtr(Y* ptr, DEL d): _ptr(ptr), _ctr_block(new ControlBlock<T>(d)){
     }
 
-    SharedPtr& operator=(const SharedPtr &r ) noexcept{
-        if(this == &r)
+    SharedPtr &operator=(const SharedPtr &r) noexcept {
+        if (this == &r)
             return *this;
         --(_ctr_block->_refCount);
         check_and_del();
@@ -49,27 +56,27 @@ public:
         return *this;
     }
 
-    void reset(int *ptr){
+    void reset(int *ptr) {
         --(_ctr_block->_refCount);
         check_and_del();
         _ptr = ptr;
-        _ctr_block = new ControlBlock<T>(1);
+        _ctr_block = new ControlBlock<T>();
     }
 
 
-
-    ~SharedPtr(){
+    ~SharedPtr() {
         --(_ctr_block->_refCount);
         check_and_del();
     }
 
 private:
-    void check_and_del(){
-        if(_ctr_block->_refCount != 0)
+    void check_and_del() {
+        if (_ctr_block->_refCount != 0)
             return;
         _ctr_block->deleter(_ptr);
         delete _ctr_block;
     }
+
 private:
     T *_ptr;
     ControlBlock<T> *_ctr_block;
--- CommonExamples/SharedPtrTest.cpp
+++ CommonExamples/SharedPtrTest.cpp
@@ -2,12 +2,37 @@
 #include <memory>
 #include <iostream>
 
-void f(int *ptr){
+void f(int *ptr) {
     delete ptr;
     //return true;
 }
-int main(){
-    int* a = new int(100);
-    SharedPtr<int> aptr(a);
+
+struct Test{
+    Test():v(1){
+        std::cout<<"Test()"<<std::endl;
+    }
+
+    explicit Test(int val):v(val){
+        std::cout<<"Test(int val)"<<std::endl;
+    }
+
+    ~Test(){
+        std::cout<<"~Test() val = "<<v<<std::endl;
+    }
+
+    int v;
+};
+
+
+
+int main() {
+    SharedPtr<Test> stptr(new Test());
+    //
+    auto deleter = [](Test *ptr){
+        std::cout<<"deleter"<<std::endl;
+        delete ptr;
+    };
+    SharedPtr<Test> stptr2(new Test(10), deleter);
+
     return 0;
 }
